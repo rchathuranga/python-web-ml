c55ba08a4b1f649f5ea85b14730c49e83f6bd013
Ravindu Chathuranga <46994483+rchathuranga@users.noreply.github.com>
2021-01-02 10:15:58 +0530

    Complete the process find DOB & Gender by NIC

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from flask import Flask
+from flask import request
+from nic_parser.parser import Parser
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def end_point():
-    return "Hello World 2"
+    name = f"{request.args['name']}"
+    nic = f"{request.args['nic']}"
 
+    np = Parser(nic)
+    dob = np.birth_date.strftime("%m/%d/%Y")
+    gender = np.gender
+
+    response = {'name': name, 'nic': nic, 'dob': dob, 'gender': f"{gender}"}
+
+    return f"{response}"
 
 if __name__ == '__main__':
     app.run(port=8081)
